081984772bf962c8f899a49ff62974173254254a
HarisKhan0 <71156152+HarisKhan0@users.noreply.github.com>
2022-11-28 13:56:05 -0800

    ver2

--- HastyPastry.java
+++ HastyPastry.java
@@ -11,3 +11,35 @@ import java.util.LinkedHashMap;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
+
+public class HastyPastry {
+    public static void main(String[] args) {
+        try {
+            HastyPastry hp = new HastyPastry();
+            int demoNum = Integer.parseInt(args[0]);
+
+            switch (demoNum) {
+                case 1:
+                    hp.demo1();
+                    break;
+                case 2:
+                    hp.demo2();
+                    break;
+                case 3:
+                    hp.demo3();
+                    break;
+                case 4:
+                    hp.demo4();
+                    break;
+                case 5:
+                    hp.demo5();
+                    break;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        } catch (Exception e2) {
+            System.err.println("Exception: " + e2.getMessage());
+        }
+    }
+}
